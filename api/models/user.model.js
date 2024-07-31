540d18cfb8a1b286d14ff729e1b76fdcbf632677
Himanshu Gupta <guptasitapur489@gmail.com>
2024-07-31 12:25:24 +0530

    Created User Schema

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -11,6 +11,10 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
       unique: true,
+      match: [
+        /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
+        "Please enter a valid email address"
+      ]
     },
     password: {
       type: String,
@@ -20,6 +24,23 @@ const userSchema = new mongoose.Schema(
       type: String,
       default:
         "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
+    },
+    bio: {
+      type: String,
+      default: "",
+    },
+    banner: {
+      type: String,
+      default:
+        "https://via.placeholder.com/1500x500.png?text=Default+Banner",
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    updatedAt: {
+      type: Date,
+      default: Date.now,
     },
   },
   { timestamps: true }
