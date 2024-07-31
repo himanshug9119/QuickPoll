41704691687769403f344276301e54729e580c41
Himanshu Gupta <guptasitapur489@gmail.com>
2024-07-31 15:43:49 +0530

    Updated signin controller

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -2,7 +2,7 @@ import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs'
 import {errorHandler} from '../utils/error.js'
 import jwt from 'jsonwebtoken'
-const JWT_SECRET = process.env.SECRET_KEY;
+const SECRET_KEY = process.env.SECRET_KEY;
 const hash_password_times = 10;
 
 // signup controller
@@ -36,31 +36,46 @@ export const signup = async (req, res, next) => {
     }
 };
 
-export const signin = async (req , res, next)=>{
-    const {email, password} = req.body;
-    if(!email || !password){
-        res.status(400).json({"Message":"Please provide username and password"})
-    }
-    let user ;
-    try{
-        const validUser = await User.findOne({ email });
-        if (!validUser) return next(errorHandler(404 , 'User Not found'));
-        const validPassword = bcryptjs.compareSync(password, validUser.password);
-        if (!validPassword) return next(errorHandler(401, "Invalid Credentials!"));
-        
-        const token = jwt.sign({id:validUser._id}, JWT_SECRET);
-        const {password:pass, ...rest} = validUser._doc;
-        res.cookie('access_token', token , {httpOnly:true}).status(200).json(rest);
-    }catch(err){
-        next(err)
-    }
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // Validate input
+  if (!email || !password) {
+      return res.status(400).json({ message: 'Please provide email and password' });
+  }
+
+  try {
+      // Find user by email
+      const validUser = await User.findOne({ email });
+      if (!validUser) {
+          return next(errorHandler(404, 'User not found'));
+      }
+
+      // Compare password
+      const validPassword = bcryptjs.compareSync(password, validUser.password);
+      if (!validPassword) {
+          return next(errorHandler(401, 'Invalid credentials'));
+      }
+
+      // Generate JWT token
+      const token = jwt.sign({ id: validUser._id }, SECRET_KEY, { expiresIn: '1h' });
+
+      // Exclude password from response
+      const { password: pass, ...userWithoutPassword } = validUser._doc;
+
+      // Set token in cookies and send response
+      res.cookie('access_token', token, { httpOnly: true, maxAge: 3600000 }); // Cookie expires in 1 hour
+      res.status(200).json(userWithoutPassword);
+  } catch (err) {
+      next(err);
+  }
 };
 
 export const google = async (req , res, next)=>{
     try {
         const user = await User.findOne({email:req.body.email})
         if(user){
-            const token = jwt.sign({ id: user._id }, JWT_SECRET);
+            const token = jwt.sign({ id: user._id }, SECRET_KEY);
             const { password: pass, ...rest } = user._doc;
             res.cookie('access_token', token , {httpOnly:true}).status(200).json(rest);
         }else{
