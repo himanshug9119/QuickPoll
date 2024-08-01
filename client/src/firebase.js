6af510ab83fca2daec3720a412e75834a5039dcd
Himanshu Gupta <guptasitapur489@gmail.com>
2024-08-01 14:13:41 +0530

    Updated firebase configurations

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,10 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import dotenv from 'dotenv'
-dotenv.config();
-const apiKey = process.env.REACT_APP_FIREBASE_API_KEY;
-// console.log(apiKey);
+
+const apiKey = import.meta.env.VITE_FIREBASE_API_KEY;
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
