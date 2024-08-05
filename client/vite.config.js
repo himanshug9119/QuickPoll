44c5438ce4e0aef929870f424eb1d0fd316f98ac
Himanshu Gupta <guptasitapur489@gmail.com>
2024-08-05 15:56:46 +0530

    Updated vite configuration file after getting error when trying to refresh the profile page

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-
+import {rewriteAll} from 'vite-plugin-rewrite-all'  
 // https://vitejs.dev/config/
 export default defineConfig({
   server:{
@@ -11,5 +11,8 @@ export default defineConfig({
       },
     },
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    rewriteAll()
+  ],
 })
