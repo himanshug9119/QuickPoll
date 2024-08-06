101bccfc48e7111979381be1d6c08ea3161277d7
Himanshu Gupta <guptasitapur489@gmail.com>
2024-08-06 06:21:31 +0530

    Updated verifyuser secretkey error and correctly imported from .env file

--- api/utils/verifyUser.js
+++ api/utils/verifyUser.js
@@ -1,9 +1,13 @@
 import { errorHandler } from "./error.js";
 import jwt from 'jsonwebtoken'
+import dotenv from 'dotenv'
+dotenv.config();
+const SECRET_KEY = process.env.REACT_APP_SECRET_KEY;
+
 export const verifyToken = (req, res, next) =>{
     const token = req.cookies.access_token;
     if(!token) return next(errorHandler(401, 'Unauthorized'))
-    jwt.verify(token, process.env.SECRET_KEY, (err, user) => {
+    jwt.verify(token, SECRET_KEY, (err, user) => {
       if (err) return next(errorHandler(403, "Forbidden"));
       req.user = user;
       next();
