5a171b89ac1650aee188d599815842559e3abebd
Himanshu Gupta <guptasitapur489@gmail.com>
2024-08-24 01:24:32 +0530

    Updated vote model now optionId attribute is also a objectId

--- api/models/vote.model.js
+++ api/models/vote.model.js
@@ -1,10 +1,14 @@
-import mongoose from "mongoose";    
+import mongoose from "mongoose";
 
 const voteSchema = new mongoose.Schema({
-    pollId: { type: mongoose.Schema.Types.ObjectId, ref: "Poll", required: true },
-    votedBy: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-    optionId: { type: Number, required: true },
-    votedAt: { type: Date, default: Date.now },
+  pollId: { type: mongoose.Schema.Types.ObjectId, ref: "Poll", required: true },
+  votedBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  optionId: { type: mongoose.Schema.Types.ObjectId, required: true }, 
+  votedAt: { type: Date, default: Date.now },
 });
 
 const Vote = mongoose.model("Vote", voteSchema);
