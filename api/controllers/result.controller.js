4272dd50b8e8248b4e3288bd93bba7a98bb2b519
Himanshu Gupta <guptasitapur489@gmail.com>
2024-08-26 15:27:56 +0530

    Updated result endpoint to return an array of objects which has username and optionId

--- api/controllers/result.controller.js
+++ api/controllers/result.controller.js
@@ -1,20 +1,38 @@
 import mongoose from "mongoose";
 import Vote from "../models/vote.model.js";
+import User from "../models/user.model.js";
 
 // Get poll results by poll ID
 export const getResult = async (req, res, next) => {
   const { pollId } = req.params;
+
   // Validate input
   if (!mongoose.Types.ObjectId.isValid(pollId)) {
     return res.status(400).json({ message: "Invalid poll ID" });
   }
 
   try {
-    // Aggregate votes by optionId
+    // Aggregate votes by optionId and include the username of the user who voted
     const results = await Vote.aggregate([
       { $match: { pollId: new mongoose.Types.ObjectId(pollId) } },
-      { $group: { _id: "$optionId", totalVotes: { $sum: 1 } } },
-      { $sort: { _id: 1 } }, // Sort by optionId
+      {
+        $lookup: {
+          from: "users",
+          localField: "votedBy",
+          foreignField: "_id",
+          as: "userDetails",
+        },
+      },
+      {
+        $unwind: "$userDetails",
+      },
+      {
+        $project: {
+          optionId: 1,
+          username: "$userDetails.username",
+        },
+      },
+      { $sort: { optionId: 1 } }, // Sort by optionId
     ]);
 
     console.log(results || "No results found");
