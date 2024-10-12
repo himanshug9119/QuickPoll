132b6b9aebf35dcf51deb254d657a0f66f1ea3e1
Himanshu Gupta <guptasitapur489@gmail.com>
2024-10-12 17:08:33 +0530

    Some update

--- api/index.js
+++ api/index.js
@@ -67,7 +67,7 @@ app.use((err, req, res, next) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
